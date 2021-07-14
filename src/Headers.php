a40572230956c47cfebd850b89c6f77f30f25937
Brooke Bryan <brooke@bajb.net>
2021-07-14 12:12:03 +0100

    Header validation

--- src/Headers.php
+++ src/Headers.php
@@ -14,4 +14,31 @@ class Headers
   const Authorization = "x-kx-authorization";// Json Authorizations, [{"k":"permission-key","e":"A","r":"*"},"k":"permission-key-2","e":"D","r":"uid1"]
   const Authentication = "x-kx-authentication";// JSON access credentials, provided by the app e.g. {"accessToken":"xx"}
 
+  public static function verify(array $headers, $signatureKey, $maxTimeDiff = 60): bool
+  {
+
+    function headerValue($headers, $key, $default = '')
+    {
+      $value = $headers[$key] ?? $default;
+      return is_array($value) ? implode('', $value) : $value;
+    }
+
+    [$signature, $timestamp] = explode('/', headerValue($headers, static::Signature, '/'), 2);
+    $time = time();
+    if($timestamp > ($time + $maxTimeDiff) || $timestamp < ($time - $maxTimeDiff))
+    {
+      return false;
+    }
+
+    return $signature == hash(
+        'sha256',
+        headerValue($headers, static::WorkspaceID)
+        . headerValue($headers, static::UserID)
+        . $signatureKey
+        . headerValue($headers, static::TraceID)
+        . headerValue($headers, static::UserIP)
+        . headerValue($headers, static::UserAgent)
+        . $timestamp
+      );
+  }
 }
