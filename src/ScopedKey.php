35f4ab86ca78a8523d1606a3495fc4422cc782f5
Brooke Bryan <brooke@bajb.net>
2024-04-24 12:02:24 +0100

    Support full scoped key

--- src/ScopedKey.php
+++ src/ScopedKey.php
@@ -6,10 +6,14 @@ class ScopedKey
   public GlobalAppID $app;
   public string $key;
 
-  public static function create($key)
+  public static function create(string $key, string $vendor = null, string $app = null)
   {
     $pk = new static();
     $pk->key = $key;
+    if($vendor != null || $app != null)
+    {
+      $pk->app = GlobalAppID::create($vendor, $app);
+    }
     return $pk;
   }
 }
