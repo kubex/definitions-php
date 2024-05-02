5724c3dcf15ecb0c0e6349f90903083fa4978724
Brooke Bryan <brooke@bajb.net>
2024-05-02 12:12:03 +0100

    Remove app nav integration point

--- src/IntegrationLocation.php
+++ src/IntegrationLocation.php
@@ -4,7 +4,6 @@ namespace Kubex\Definitions;
 class IntegrationLocation
 {
   public const PageNav = "nav.page";
-  public const AppNav = "nav.app";
   public const Action = "action";
   public const Panel = "panel";
 }
