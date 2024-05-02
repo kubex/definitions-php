287c047944cf09bb32f41d157904272a9a8a05d3
Brooke Bryan <brooke@bajb.net>
2024-05-02 12:16:01 +0100

    Add integration location id

--- src/IntegrationPoint.php
+++ src/IntegrationPoint.php
@@ -3,11 +3,12 @@ namespace Kubex\Definitions;
 
 class IntegrationPoint
 {
-  public GlobalAppID $integrateApp;
+  public GlobalAppID $integrateApp; // Which app to integrate into
   /** @var string|IntegrationLocation */
-  public string $location;
-  public string $pathID;
-  public EntryPoint $entryPoint;
+  public string $location; // Where to place the integration
+  public string $locationID; // Location ID if multiple locations available
+  public string $pathID; //Remote app path ID
+  public EntryPoint $entryPoint; // How the integration is presented
   /** @var array<ScopedKey> */
-  public array $requiredPermissions = [];
+  public array $requiredPermissions = [];// Permissions that must be set for the user to see this item
 }
