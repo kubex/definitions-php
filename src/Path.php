c8149edf7b0b9e8373f5abc72aa6466938d0121d
Brooke Bryan <brooke@bajb.net>
2024-05-13 08:20:17 +0100

    Add hover

--- src/Path.php
+++ src/Path.php
@@ -3,15 +3,17 @@ namespace Kubex\Definitions;
 
 class Path
 {
-  public string $id;
-  public string $path;
-  public string $method;
+  public string $id; // Allow the path to be linked
+  public string $path; // with replacements, matches start, locating the most specific
+  public string $method; // HTTP Method, blank for any
 
   public Text $name;
   public Text $description;
-  public bool $hideHeader = false;
-  public bool $hideBreadcrumb = false;
-  public bool $promptOnExit = false;
+
+  public bool $hideHeader = false; // Hide the header when this path is active
+  public bool $hideBreadcrumb = false; // Hide the breadcrumb when this path is active
+  public bool $promptOnExit = false; // Prompt the user when they try to leave the page
+  public bool $hoverCard = false; // If this path can be displayed as a hoverCard
 
   /** @var array<ScopedKey> */
   public array $requestPermissions = [];
