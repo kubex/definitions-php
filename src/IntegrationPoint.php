b44c3e4c2544a5984ec64a69022b5ce86295739c
Brooke Bryan <brooke@bajb.net>
2024-05-30 18:31:30 +0100

    Add panel actions & tabs

--- src/IntegrationPoint.php
+++ src/IntegrationPoint.php
@@ -9,6 +9,12 @@ class IntegrationPoint
   public string $locationID; // Location ID if multiple locations available
   public string $pathID; //Remote app path ID
   public EntryPoint $entryPoint; // How the integration is presented
+
+  /** @var array<EntryPoint> */
+  public array $panelActions = [];  // Actions to add to the panel
+  /** @var array<EntryPoint> */
+  public array $panelTabs = [];  // Tabs to show - panels only
+
   /** @var array<ScopedKey> */
   public array $requiredPermissions = [];// Permissions that must be set for the user to see this item
 }
