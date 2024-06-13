3a84875469168a7f5576961a304a726e5b41ef9b
Brooke Bryan <brooke@bajb.net>
2024-06-13 22:25:18 +0100

    Integrate with version

--- src/IntegrationPoint.php
+++ src/IntegrationPoint.php
@@ -3,7 +3,7 @@ namespace Kubex\Definitions;
 
 class IntegrationPoint
 {
-  public GlobalAppID $integrateApp; // Which app to integrate into
+  public ScopedKey $integrateApp; // Which app to integrate into
   /** @var string|IntegrationLocation */
   public string $location; // Where to place the integration
   public string $locationID; // Location ID if multiple locations available
