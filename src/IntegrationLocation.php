a12daeb0813fc97d711a73075eaa8048ee93db02
Kyle Essex <kylegavinessex@hotmail.com>
2024-08-06 12:00:49 +0100

    Update Integration Points

--- src/IntegrationLocation.php
+++ src/IntegrationLocation.php
@@ -3,7 +3,12 @@ namespace Kubex\Definitions;
 
 class IntegrationLocation
 {
-  public const PageNav = "nav.page";
-  public const Action = "action";
-  public const Panel = "panel";
+  public const Page = "page"; // Top Level Page Navigation
+  public const Tab = "tab"; // Header Navigation on Page
+  public const Action = "action"; // <app-actions>
+  public const Panel = "panel"; // <app-panel>
+
+
+  // @deprecated
+  public const PageNav = self::Tab; // Header Navigation on Page
 }
