4718a27393479096f67641636020ea760d164e0c
Kyle Essex <kylegavinessex@hotmail.com>
2025-01-13 16:16:29 +0000

    Add multi panel

--- src/IntegrationPoint.php
+++ src/IntegrationPoint.php
@@ -9,7 +9,7 @@ class IntegrationPoint
   public string $locationID; // Location ID if multiple locations available
   public string $pathID; //Remote app path ID
   public EntryPoint $entryPoint; // How the integration is presented
-  public bool $mutliPanel; // Whether the integration can be shows multiple panels
+  public bool $multiPanel; // Whether the integration can be shows multiple panels
 
   public int $preferredWidth; // Preferred width of the integration
 
