e2133553b05e8ceb51b0f2034611d9a527a9e46e
Brooke Bryan <brooke@bajb.net>
2025-01-15 15:40:27 +0000

    Add Path constants

---
+++ src/Paths.php
@@ -0,0 +1,8 @@
+<?php
+namespace Kubex\Definitions;
+
+const PathHealthCheck = "/_kubex/health";
+const PathDefinition = "/_kubex/definition";
+const PathAbout = "/_kubex/about";
+
+const PathPrefixWebhooks = "/_webhooks/";
