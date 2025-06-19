1f2179e27449f370d3a6f81580499babbd1a944c
Brooke Bryan <brooke@bajb.net>
2025-06-19 16:48:15 +0100

    Additional launch modes

--- src/LaunchMode.php
+++ src/LaunchMode.php
@@ -7,4 +7,6 @@ class LaunchMode
   public const Modal = "modal";    // Launch in a modal
   public const Window = "window";   // Launch in a new window
   public const Slide = "slide";  // Right side overlay app
+  public const ActionDrop = "adrop";  // Drop down from the action bar
+  public const ActionFill = "afill";  // Fill the action bar with the app
 }
