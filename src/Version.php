7c24d0be61e32c3242e14cc99c5c20c77982f666
Brooke Bryan <brooke@bajb.net>
2025-07-30 13:20:40 +0100

    Default values

--- src/Version.php
+++ src/Version.php
@@ -5,9 +5,9 @@ namespace Kubex\Definitions;
 // Version data struct to return on /_kubex/version
 class Version
 {
-  public string $environment; // Environment name (e.g., "production", "sandbox")
-  public string $version; // Application version
-  public string $buildTime; // Build date in RFC3339 format
-  public string $buildID; // Build ID
-  public string $commit; // Commit hash
+  public string $environment = ''; // Environment name (e.g., "production", "sandbox")
+  public string $version = ''; // Application version
+  public string $buildTime = ''; // Build date in RFC3339 format
+  public string $buildID = ''; // Build ID
+  public string $commit = ''; // Commit hash
 }
