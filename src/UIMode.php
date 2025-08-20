fd919c397e53a8899f7ee0bc8e9203cd8880154b
Kyle Essex <kylegavinessex@hotmail.com>
2025-08-20 17:17:59 +0100

    Add dextral and basic

--- src/UIMode.php
+++ src/UIMode.php
@@ -4,6 +4,8 @@ namespace Kubex\Definitions;
 class UIMode
 {
   public const Full = "full"; // Standard application
+  public const Dextral = "dextral"; // Dextral application
+  public const Basic = "basic"; // Simple application - used for non-native applications
   public const Integration = "integration"; // Integrated into existing pages
   public const None = "none"; // No UI - flow only
 }
