f39ef11d8a4b89d108ad9ade326ba1681015828a
Alan Jones <alan.jones@chargehive.com>
2025-09-03 11:14:32 +0100

    Add `RequestContextData` header

--- src/Headers.php
+++ src/Headers.php
@@ -30,6 +30,9 @@ class Headers
   // RequestWorkspaceHost The full workspace host the user is currently in
   const RequestWorkspaceHost = "x-kx-workspace-host"; // Host of the workspace, used for routing
 
+  // RequestAuthentication JSON context data
+  const RequestContextData = "x-kx-context-data";
+
   // ResponseDebug Debug object for the browser
   const ResponseDebug = "x-kubex-debug";
 
