96591eca2a583bd94a5d52f03ddc86da07b05531
Brooke Bryan <brooke@bajb.net>
2025-09-25 16:02:51 +0100

    Add help ui

--- src/UIMode.php
+++ src/UIMode.php
@@ -7,5 +7,6 @@ class UIMode
   public const Dextral = "dextral"; // Dextral application
   public const Basic = "basic"; // Simple application - used for non-native applications
   public const Integration = "integration"; // Integrated into existing pages
+  public const Help = "help"; // Help App
   public const None = "none"; // No UI - flow only
 }
